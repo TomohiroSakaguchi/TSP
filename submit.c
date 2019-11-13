57ac0fb620001d24e09c2ee5565fc6153beecbb7
TomohiroSakaguchi <stomohiro59@gmail.com>
2019-11-13 21:02:51 +0900

    アニーリング法を実装

--- submit.c
+++ submit.c
@@ -12,7 +12,7 @@ int main(int argc,char *argv[])
   int ncity;                    /* the total number of cities you must visit*/
   double x[MAXCITY],y[MAXCITY]; /* array for position data */
   int nroute[MAXCITY]={0},min_route[MAXCITY]={0};      /* array for route data */
-  double dis_tot,def_dis=0.0,new_dis=0.0,min_dis=3000000.0;               /* total distance */
+  double p,T,rnd,dis_tot,def_dis=0.0,new_dis=0.0,min_dis=3000000.0;               /* total distance */
 
   /* local variables */
   FILE *input, *output;
@@ -102,8 +102,8 @@ int main(int argc,char *argv[])
   srand((unsigned)time(NULL));
   nrnd = rand()%(ncity);
   second = (nrnd + 1) % ncity;
-  printf("one sity is %d\n",nrnd);
-  printf("second sity is %d\n",second);
+  //printf("one sity is %d\n",nrnd);
+  //printf("second sity is %d\n",second);
 
   /*swap city*/
   tmp = nroute[nrnd];
@@ -128,21 +128,38 @@ int main(int argc,char *argv[])
   ry = y[ii] - y[jj];
   rr = sqrt(rx*rx + ry*ry);
   new_dis = new_dis + rr;
-  printf("swapped dist.%lf\n",new_dis);
+  //printf("swapped dist.%lf\n",new_dis);
 
   if(new_dis <= min_dis){
     min_dis = new_dis;
     for(i=0 ; i < ncity ; i++){
       min_route[i] = nroute[i];
-      printf("%d\n", min_route[i]);
+      //printf("%d\n", min_route[i]);
     }
     count++;
 //    printf("これが最小やで\n");
   }
   else{
-  //  printf("これじゃない\n");
+    /* random number generator */
+    srand((unsigned)time(NULL)); 
+    rnd = (double)rand()/RAND_MAX;
+    //printf("実数の乱数%lf\n",rnd);
+    T = 8.7;
+    p = exp(-(new_dis - min_dis)/T);
+    //printf("pの値 %lf\n",p);
+    if(rnd < p ){
+     //printf("悪くなったけど採用\n"); 
+     continue;
+     //printf("continue確認\n");
+    }else{
+      /*swap city*/
+      tmp = nroute[nrnd];
+      nroute[nrnd] = nroute[second];
+      nroute[second] = tmp;
+      //printf("悪くなったので却下\n");
+    }  
   }
-  if(count==100000){
+  if(count==10000000){
     break;
   }
 
@@ -162,7 +179,7 @@ int main(int argc,char *argv[])
   /* Stop Watch END */
   cpu_time2 = clock();
   printf("minimize distance is %20.14lf\n",min_dis);
-
+  printf("temperture is %lf\n",T);
   /* Output City Data */
   printf("Optimized Route\n");
   for (i=0 ; i < ncity ; i++){
@@ -198,10 +215,10 @@ int main(int argc,char *argv[])
   fprintf(output,"%20.14f\n",(double)(cpu_time2-cpu_time1)/(double)CLOCKS_PER_SEC);
   for(i = 0; i < ncity; i++){
     j = min_route[i];
-    fprintf(output,"%d %lf %lf\n",j,x[j],y[j]);
+    //fprintf(output,"%d %lf %lf\n",j,x[j],y[j]);
   }
   j = min_route[0];
-  fprintf(output,"%d %lf %lf\n",j,x[j],y[j]);
+  //fprintf(output,"%d %lf %lf\n",j,x[j],y[j]);
   fclose(output);
 
 
